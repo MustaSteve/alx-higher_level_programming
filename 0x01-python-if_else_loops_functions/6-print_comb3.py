bc5cf4a47faf195bf8933138a406e4a711b768c5
MustaSteve <avineusq@gmail.com>
2023-08-08 10:04:21 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for di1 in range(0, 10):
     for di2 in range(di1 + 1, 10):
         if di1 == 8 and di2 == 9:
-            print("{:02d}".format(di1, di2))
+            print("{}{}".format(di1, di2))
         else:
-            print("{:02d}".format(di1, di2), end=", ")
+            print("{}{}".format(di1, di2), end=", ")
