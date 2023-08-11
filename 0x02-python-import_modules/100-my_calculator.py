00eef4a5548b37fef26d0b628d55d05b5e54683c
MustaSteve <avineusq@gmail.com>
2023-08-11 11:11:43 -0700

    Python import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,16 +6,16 @@ if __name__ == "__main__":
     if len != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    n1 = int(argv[1])
-    n2 = int(argv[3])
+    a = int(argv[1])
+    b = int(argv[3])
     if argv[2] == '+':
-        print("{:d} + {:d} = {:d}".format(n1, n2, add(n1, n2)))
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif argv[2] == '-':
-        print("{:d} - {:d} = {:d}".format(n1, n2, sub(n1, n2))) 
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     elif argv[2] == '*':
-        print("{:d} * {:d} = {:d}".format(n1, n2, mul(n1, n2)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     elif argv[2] == '/':
-        print("{:d} / {:d} = {:d}".format(n1, n2, div(n1, n2)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
