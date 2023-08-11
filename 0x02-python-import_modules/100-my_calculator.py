ba9cb54eac214bf53761c78546e91a42d0dccde4
MustaSteve <avineusq@gmail.com>
2023-08-11 10:40:59 -0700

    Python import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
-    len = len(argv)
-    if len != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+    argc = len(argv)
+    if argc != 4:
+        print('Usage: {} <a> <operator> <b>'.format(argv[0]))
+        exit(1)
+    ops = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    }
 
     if argv[2] in ops:
         a = int(argv[1])
