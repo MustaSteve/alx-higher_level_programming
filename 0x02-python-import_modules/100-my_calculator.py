f07fcd6eacea96ba05f9f555add04e75487426af
MustaSteve <avineusq@gmail.com>
2023-08-11 10:39:16 -0700

    Python import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,18 +4,22 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     len = len(argv)
     if len != 4:
-        print("Usage: ./100-my_calculator.py <num1> <operator> <num2>")
-        exit(1)
-    num1 = int(argv[1])
-    num2 = int(argv[3])
-    if argv[2] == '+':
-        print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
-    elif argv[2] == '-':
-        print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2))) 
-    elif argv[2] == '*':
-        print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
-    elif argv[2] == '/':
-        print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
-    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    if argv[2] in ops:
+        a = int(argv[1])
+        b = int(argv[3])
+        op = ops[argv[2]]
+        result = op(a, b)
+        print('{:d} {:s} {:d} = {:d}'.format(a, argv[2], b, result))
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
+    exit(0)
+    
