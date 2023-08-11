6ce1dcc6f5d045586dd5cc21713f9b377d52f031
MustaSteve <avineusq@gmail.com>
2023-08-11 09:46:53 -0700

    Python import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
     from calculator_1 import add, sub, mul, div
-    if len(sys.argv) - 1 != 4:
+    import sys
+
+
+    if len(sys.argv) - 1 != 3:
         print("Usage: {} <a> <operator> <b>")
         sys.exit(1)
     ops = {
