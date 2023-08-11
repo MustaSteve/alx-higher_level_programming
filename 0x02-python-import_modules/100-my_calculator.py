ab5b620f9cbbfc713df1ed74c2e10939b3011170
MustaSteve <avineusq@gmail.com>
2023-08-11 09:45:25 -0700

    Python import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     from calculator_1 import add, sub, mul, div
-    argc = len(argv)
-    if argc != 4:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>'.format(argv[0]))
-        exit(1)
+    if len(sys.argv) - 1 != 4:
+        print("Usage: {} <a> <operator> <b>")
+        sys.exit(1)
     ops = {
         '+': add,
         '-': sub,
         '*': mul,
         '/': div
     }
-    if argv[2] not in list(ops.keys()):
-        numbre1 = int(argv[1])
-        numbre2 = int(argv[3])
-        op = ops[argv[2]]
-        result = op(numbre1, numbre2)
-        print('{:d} {:s} {:d} = {:d}'.format(numbre1, argv[2], numbre2, result))
+    if sys.argv[2] not in list(ops.keys()):
+        nu1 = int(sys.argv[1])
+        nu2 = int(sys.argv[3])
+        print('{} {} {} = {}'.format(nu1, argv[2], nu2, ops[sys.argv[2]](nu1,nu2)))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
+        sys.exit(1)
     exit(0)
