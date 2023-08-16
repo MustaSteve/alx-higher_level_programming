581a3b241d20a302a25d5d040dbd22bccb978ab1
MustaSteve <avineusq@gmail.com>
2023-08-16 13:15:47 -0700

    Python - More Data Structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@ def square_matrix_simple(matrix=[]):
     squere_marix = []
     for i in matrix:
         inside = list(map(lambda x: x**2, i))
-        squeres.append(inside)
+        squere_marix.append(inside)
     return squere_marix
