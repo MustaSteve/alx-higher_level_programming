f22fce932f1cfad2ff4044e0603be839c4be820e
MustaSteve <avineusq@gmail.com>
2023-08-16 13:28:28 -0700

    Python - More Data Structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if roman_string is None or type(roman_string) is not str:
         return 0
-    roman_nums = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-            }
-    current_num = 0
-    num_prev = 0
-    character_before = ""
-    reslut = 0
+    my_dictionary = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    num_curr = 0
+    num_bef = 0
+    char_bef = ""
+    count = 0
     for i in roman_string:
-        if i in roman_nums:
-            current_num += roman_nums[i]
-            if num_prev < roman_nums[i]:
-                current_num -= number_prev * 2
-            num_prev = roman_nums[i]
-            if i == character_before:
-                result += 1
-                if result == 3:
+        if i in my_dictionary:
+            num_curr += my_dictionary[i]
+            if num_bef < my_dictionary[i]:
+                num_curr -= num_bef * 2
+            num_bef = my_dictionary[i]
+            if i == char_bef:
+                count += 1
+                if count == 3:
                     return 0
             else:
-                result = 0
-            character_before = i[:]
-    return current_num
+                count = 0
+            char_bef = i[:]
+    return num_curr
