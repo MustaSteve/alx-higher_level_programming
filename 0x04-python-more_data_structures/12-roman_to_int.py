ad66b0ff75108225e7687bdfbb19d9c88f965713
MustaSteve <avineusq@gmail.com>
2023-08-16 13:50:23 -0700

    Python - More Data Structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    my_dictionary = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    num_curr = 0
-    num_bef = 0
-    char_bef = ""
-    count = 0
-    for i in roman_string:
-        if i in my_dictionary:
-            num_curr += my_dictionary[i]
-            if num_bef < my_dictionary[i]:
-                num_curr -= num_bef * 2
-            num_bef = my_dictionary[i]
-            if i == char_bef:
-                count += 1
-                if count == 3:
-                    return 0
-            else:
-                count = 0
-            char_bef = i[:]
-    return num_curr
+    roman_nums = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+    result_i = 0
+    value_bef = 0
+
+    for charc in reversed(roman_string):
+        value_i = roman_nums.get(charc, 0)
+        if value_i >= value_bef:
+            result_i += value_i
+        else:
+            result_i -= value_i
+        value_bef = value_i
+    return result_i
