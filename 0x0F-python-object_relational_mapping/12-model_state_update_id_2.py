f1293e737eccdc6e50d73e6ff0629d5f017f7da9
MustaSteve <avineusq@gmail.com>
2023-11-24 06:55:27 -0800

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,38 +1,18 @@
 #!/usr/bin/python3
 """
-Script to Update object in database
+lists the first state
 """
-
+from model_state import State, Base
 from sys import argv
-from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy import text
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
 
-    # Connect to the database
-    engine = create_engine(
-        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}",
-        pool_pre_ping=True
-    )
-
-    Base.metadata.create_all(engine)
-
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}\
+            @localhost/{}".format(argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    # Update the State object in the database
-    states = session.query(State).get(2)
-    states.name = "New Mexico"
-    session.add(states)
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
     session.commit()
-
-    # Output Query
-    states = session.query(State)
-
-    # Print the new state's id
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-
